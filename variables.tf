41abcd06754827a486c6d06ddd3275aa255127c0
SINGH SNEH <25021804@mahindra.com>
2024-02-19 10:12:05 +0530

    intial setup of instance scheduler

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "name" {
+    type=string
+    description = "name of policy"  
+}
+variable "project_id" {
+    type = string 
+    description = "project id "  
+}
+variable "region" {
+    type=string
+    description = "Region of policy " 
+}
+variable "start_time" {
+    type=string
+    description = "start time of vm" 
+}
+variable "stop_time" {
+    type=string
+    description = "stop time of vm "  
+}
